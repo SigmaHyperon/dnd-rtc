653f9df862168521e215ebea420d69a01ef6b876
SigmaHyperon <nano_tech@web.de>
2019-03-01 13:50:14 +0100

    fixed notification logo path

--- client/res/js/coms.js
+++ client/res/js/coms.js
@@ -81,7 +81,7 @@ function connect(id){
         } else {
             gui.showMessage(data);
             notify('DnD Message', {
-                icon: './res/img/logo.png',
+                icon: './../res/img/logo.png',
                 body: `${data.sender.name} says: \n${data.text}`,
                 vibrate: [300, 100, 400]
             });
