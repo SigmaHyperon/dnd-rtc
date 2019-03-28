81aedb6a2c4785c354a51cceac18d3c97275cc6a
SigmaHyperon <nano_tech@web.de>
2019-03-28 13:57:10 +0100

    added ssl config

---
+++ ssl/mkcert.bat
@@ -0,0 +1,3 @@
+@echo off
+openssl req -x509 -sha256 -nodes -newkey rsa:2048 -days 365 -config cert.cnf -keyout localhost.key -out localhost.crt
+openssl pkcs12 -export -name "localhost" -out localhost.pfx -inkey localhost.key -in localhost.crt -password pass:password
